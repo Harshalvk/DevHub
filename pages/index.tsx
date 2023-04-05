43477b34efd90d77a82170e6b189b9dacd5b0a97
Harshal-0816 <harshalvkhobragade@gmail.com>
2023-04-05 18:46:01 +0530

    Done Frontend of Sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div className='text-white text-3xl'>Hello world</div>
+    <div className='text-sky-300 text-3xl'>Hello Harshal!</div>
   )
 }
 
