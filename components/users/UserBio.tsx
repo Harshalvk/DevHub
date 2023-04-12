1a5c78580acaa4c629e7d0501f2a08a679bc0a9f
Harshal-0816 <harshalvkhobragade@gmail.com>
2023-04-13 00:51:04 +0530

    Bio, Profile/Cover image Functionality

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,7 @@ import useUser from '@/hooks/useUser';
 import { useMemo } from 'react';
 import Button from '../Button';
 import { BiCalendar } from 'react-icons/bi';
+import useEditModal from '@/hooks/useEditModal';
 
 interface UserBioProps {
   userId: string;
@@ -13,6 +14,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
+
+  const editModal = useEditModal();
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
@@ -26,7 +29,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     <div className='border-b-[1px] border-neutral-800 pb-4'>
       <div className='flex justify-end p-2'>
         {currentUser?.id === userId ? (
-          <Button secondary label='Edit' onClick={() => {}} />
+          <Button secondary label='Edit' onClick={editModal.onOpen} />
         ) : (
           <Button 
             onClick={() => {}}
@@ -37,7 +40,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
       </div>
       <div className='mt-8 px-4'>
           <div className='flex flex-col'>
-            <p className='text-white text-2xl font-semibold'>{fetchedUser?.name}
+            <p className='text-white text-2xl font-semibold'>
+              {fetchedUser?.name}
             </p>
             <p className='text-md text-neutral-500'>
               @{fetchedUser?.username}
@@ -86,4 +90,4 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   );
 }
 
-export default UserBio
+export default UserBio;
