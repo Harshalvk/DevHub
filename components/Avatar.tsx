cb9e778df3824961f98405fdfa7b378da6d5b7b2
Harshal-0816 <harshalvkhobragade@gmail.com>
2023-04-13 01:00:32 +0530

    Fix avatar border

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -28,7 +28,7 @@ const Avatar: React.FC<AvatarProps> = ({
 
   return (
     <div className={`
-      ${hasBorder ? 'border-black' : ''}
+      ${hasBorder ? 'border-4 border-black' : ''}
       ${isLarge ? 'h-32' : 'h-12'}
       ${isLarge ? 'w-32' : 'w-12'}
       rounded-full
