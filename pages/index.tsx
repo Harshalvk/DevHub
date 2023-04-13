8a0618272308afe2f97e000f171908c6834197d1
Harshal-0816 <harshalvkhobragade@gmail.com>
2023-04-13 12:36:16 +0530

    Add tweet functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import Header from '../components/Header';
+import Form from '@/components/Form';
 
 const index = () => {
   return (
     <>
       <Header label='Home'/>
+      <Form placeholder="What's happening?" />
     </>
   )
 }
