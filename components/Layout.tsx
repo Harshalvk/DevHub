7bd8bb9d48be86b572ab240df8bd3f125bffdf21
Harshalvk <harshalvkhobragade@gmail.com>
2024-07-29 18:23:12 +0530

    FIX: where whole app was scrolling, now only the posts section scrolls

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -32,7 +32,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           <div className="container h-full mx-auto xl:px-30 max-w-6xl">
             <div className="grid grid-cols-4 h-full">
               <Sidebar />
-              <div className="col-span lg:col-span-2 border-x-[1px] border-neutral-800">
+              <div className="col-span lg:col-span-2 border-x-[1px] border-neutral-800 overflow-y-scroll no-scrollbar">
                 {children}
               </div>
               <FollowBar />
